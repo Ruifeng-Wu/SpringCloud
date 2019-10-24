e6773e78e91552e6e8d63ff7662e06d3c940a271
ruifeng.wu <ruifeng.wu@thoughtworks.com>
2019-10-24 18:20:38 +0800

    update

--- config-client/src/main/java/com/example/config/client/ConfigClientApplication.java
+++ config-client/src/main/java/com/example/config/client/ConfigClientApplication.java
@@ -1,10 +1,10 @@
 package com.example.config.client;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class ConfigClientApplication {
 
